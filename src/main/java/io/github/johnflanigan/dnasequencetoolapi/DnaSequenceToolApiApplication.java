ef62fb1acee26d17f3450bb49d19a9e933edd4be
John Flanigan <johnrflanigan@gmail.com>
2020-08-27 17:02:18 -0400

    Initial commit

---
+++ src/main/java/io/github/johnflanigan/dnasequencetoolapi/DnaSequenceToolApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.johnflanigan.dnasequencetoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaSequenceToolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaSequenceToolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/johnflanigan/dnasequencetoolapi/DnaSequenceToolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johnflanigan.dnasequencetoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaSequenceToolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
