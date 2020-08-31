7d6de75b83cd53dc1b5373f1d87b71c72b673e4c
John Flanigan <johnrflanigan@gmail.com>
2020-08-31 16:42:09 -0400

    Remove unused endpoints, fix cross origins

--- src/main/java/io/github/johnflanigan/dnasequencetoolapi/SequenceController.java
+++ src/main/java/io/github/johnflanigan/dnasequencetoolapi/SequenceController.java
@@ -1,8 +1,10 @@
 package io.github.johnflanigan.dnasequencetoolapi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,21 +12,19 @@ import java.util.Optional;
 @RestController
 public class SequenceController {
 
-    private static final Logger log = LoggerFactory.getLogger(SequenceController.class);
-
     private final SequenceRepository repository;
 
     public SequenceController(SequenceRepository repository) {
         this.repository = repository;
     }
 
-    @CrossOrigin(origins = {"http://localhost:3000", "https://dna-sequence-tool.herokuapp.com/"})
+    @CrossOrigin(origins = {"http://localhost:3000", "https://dna-sequence-tool.herokuapp.com"})
     @GetMapping("/sequences")
     List<Sequence> all() {
         return repository.findAll();
     }
 
-    @CrossOrigin(origins = {"http://localhost:3000", "https://dna-sequence-tool.herokuapp.com/"})
+    @CrossOrigin(origins = {"http://localhost:3000", "https://dna-sequence-tool.herokuapp.com"})
     @PostMapping("/sequences")
     Sequence newSequence(@RequestBody Sequence newSequence) {
         Optional<Sequence> sequenceOptional = repository.findFirstBySequence(newSequence.getSequence());
@@ -34,33 +34,5 @@ public class SequenceController {
         } else {
             return repository.save(newSequence);
         }
-    }
-
-    @GetMapping("/sequences/{id}")
-    Sequence one(@PathVariable Long id) {
-
-        return repository.findById(id)
-                .orElseThrow(() -> new SequenceNotFoundException(id));
-    }
-
-    @PutMapping("/sequences/{id}")
-    Sequence replaceSequence(@RequestBody Sequence newSequence, @PathVariable Long id) {
-
-        return repository.findById(id)
-                .map(sequence -> {
-                    sequence.setName(newSequence.getName());
-                    sequence.setDescription(newSequence.getDescription());
-                    sequence.setSequence(newSequence.getSequence());
-                    return repository.save(sequence);
-                })
-                .orElseGet(() -> {
-                    newSequence.setId(id);
-                    return repository.save(newSequence);
-                });
-    }
-
-    @DeleteMapping("/sequences/{id}")
-    void deleteEmployee(@PathVariable Long id) {
-        repository.deleteById(id);
     }
 }
